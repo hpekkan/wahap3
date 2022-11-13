736c7fabba1d7f96e5bf896aa8f7e1ff737d8e18
berykay <berkayyildiz550@hotmail.com>
2022-11-13 10:50:51 +0300

    form adding is going on

--- webapp/src/views/IndexSections/Form.js
+++ webapp/src/views/IndexSections/Form.js
@@ -1,9 +1,45 @@
 import React from "react";
+import {Input ,Button, Container, Row, Col } from "reactstrap";
 
 export default function Form() {
     return (
         <div>
-            <h1>Form</h1>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <Container>
+                    <Col lg="10" md="12"></Col>
+                Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+                Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+                    Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+                    Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+                    Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+                    Yayınlayan kişi:
+                    <Input defaultValue="" placeholder="Regular" type="text" />
+           
+           <br></br>
+           
+            <Row className="justify-content-center">    
+            <Button
+              className="btn-round"
+              color="red"
+              role="button"
+              size="lg"
+            >
+              Onayla
+            </Button>
+            </Row>
+
+        
+      </Container>
         </div>
     );
 }
